9015bb7b62af4ae7c7b0caa7670ece27587c02f1
TrTeeter <59981794+TrTeeter@users.noreply.github.com>
2020-04-07 14:10:24 -0500

    Tried converting changes into a class

--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -14,10 +14,11 @@ namespace PersonalProjectLab
 
 
             string line = ("");
-            int test;
+           
             string productreq;
-            SortedList<string, int> name;
-            SortedList<string,int> products = new SortedList<string,int>();
+           
+
+            List<Tuple<string, int>> products = new List<Tuple<string, int>>();
 
             Console.WriteLine("WELCOME TO T.A.P.C.");
             Console.WriteLine("'Totally Awesome Price Comparitor'");
@@ -25,42 +26,50 @@ namespace PersonalProjectLab
             //hard code an import list of items and prices
       
 
-            if (File.Exists("datafile.txt"))
+            if (File.Exists("importeddata.txt"))
             {
                 Console.WriteLine("test");
-                using (StreamReader sr = File.OpenText("datafile.txt"))
+                using (StreamReader sr = File.OpenText("importeddata.txt"))
                 {
                     
                   
                     while ((line = sr.ReadLine()) != null)
                     {
+                        string[] values = line.Split(',');
+                        products.Add(new Tuple<string, int>(values[0], int.Parse(values[1])));
 
+                       
 
-                      
-
-                        
-
-                        Console.WriteLine(line);
-                        
                     }
 
 
                 }
 
             }
-            Console.WriteLine(products);
+            
 
             //create a class that runs through the document and adds each object into a bank to search from that sorts them by name, then price then lists the source
             //If the price is better than replace with the lowest price
             //ask the user for the name of the product they are looking for
+
             Console.WriteLine("What Can I help you find today?");
             productreq= Console.ReadLine();
-           
-        //Using looping, Display the lowest price by running through the classes and if the price is equal or lower replace it as the display product
-        //While doing this loop is happing, create an average price index to display for information about the product.
-        //If the product isn’t shown redirect to the name of the product
-        // display the product, price, average price, and store
-        //ask the user if they would like to repeat the process.
-    }
+
+            foreach (var productItem in products)
+            {
+                if (productItem.Item1 == productreq)
+                {
+                    Console.WriteLine(productreq + "= $" + productItem.Item2);
+                }
+            }
+            //create class that selects object and best price
+
+
+            //Using looping, Display the lowest price by running through the classes and if the price is equal or lower replace it as the display product
+            //While doing this loop is happing, create an average price index to display for information about the product.
+            //If the product isn’t shown redirect to the name of the product
+            // display the product, price, average price, and store
+            //ask the user if they would like to repeat the process.
+        }
     }
 }
---
+++ PersonalProjectLab/PersonalProjectLab/SortingEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PersonalProjectLab
+{
+    public class SearchEngine
+    {
+        public class
+        {
+            foreach (var productItem in _products)
+            {
+                if (productItem.Item1 == _productreq)
+                {
+                    Console.WriteLine(_productreq + "= $" + productItem.Item2);
+                }
+            }
+        }
+    }
+}
