26b60a5c8dd7dca7aa1e75e444126b021388b45c
TrTeeter <59981794+TrTeeter@users.noreply.github.com>
2020-04-07 15:26:19 -0500

    Update SortingEngine.cs

--- PersonalProjectLab/PersonalProjectLab/SortingEngine.cs
+++ PersonalProjectLab/PersonalProjectLab/SortingEngine.cs
@@ -1,17 +1,60 @@
 ﻿using System;
-namespace PersonalProjectLab
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingEngine
 {
     public class SearchEngine
     {
-        public class
+        List<Tuple<string, int>> _products;
+        string _productreq;
+        
+
+
+        public void Importlist(List<Tuple<string,int>> products)
         {
+            products = _products;
+        }
+
+        public void importuserinput(string productreq)
+        {
+            productreq = _productreq;
+        }
+        private List<Tuple<string, int>> SearchResults()
+        {
+
+            int sum = 0;
+            int numberofproducts = 0;
+            List<Tuple<string, int>> SearchResults;
+
             foreach (var productItem in _products)
             {
                 if (productItem.Item1 == _productreq)
                 {
-                    Console.WriteLine(_productreq + "= $" + productItem.Item2);
+
+                     SearchResults += productItem;
+                    
                 }
             }
+            
+        }
+       public int average()
+        {
+
+            int sum = 0;
+            int numberofproducts = 0;
+            foreach (var productItem in _products)
+            {
+                if (productItem.Item1 == _productreq)
+                {
+
+                    sum += productItem.Item2;
+                    numberofproducts++;
+                }
+            }
+            int avg = sum / numberofproducts;
+            return avg;
         }
     }
 }
